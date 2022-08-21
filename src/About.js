bef5cfdc1e6501c89aaf8aab45cb2af73475614a
Alex <alex3334@gmail.com>
2022-08-21 23:18:15 +0200

    fixing scaling

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="card fadeIn">
       <h2>About this app</h2>
-      <p style={{ fontWeight: 100, fontStyle: "italic" }}>
+      <p style={{ fontWeight: 200 }}>
         This app was made to meet my needs in terms of sport timers. I hope you
         like it too. My main goal was to keep everything simple and provide
         basic set of timers such as Tabata / HIIT Timer.
